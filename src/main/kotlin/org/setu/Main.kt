e61c1c233f4daadfc753ed65cce56b2b03b08f5d
Adam Raher <40344154+AdamRashers@users.noreply.github.com>
2024-12-07 21:34:55 +0000

    Added a quiz to test variables being saved to an xml file. Contents of main.kt will be deleted and started over. I am only trying to get my bearing on this project and where I should start

--- src/main/kotlin/org/setu/Main.kt
+++ src/main/kotlin/org/setu/Main.kt
@@ -1,6 +1,34 @@
 package org.setu
 
-fun main() {
-    println("Welcome to Insurance4You")
-}
+import java.io.File
 
+fun main() {
+    println("Enter Tech details:")
+    print("Name: ")
+    val name = readLine() ?: ""
+    print("Email: ")
+    val email = readLine() ?: ""
+    print("Phone: ")
+    val phone = readLine() ?: ""
+    print("County: ")
+    val county = readLine() ?: ""
+    print("Tech Level: ")
+    val techLevel = readLine()?: ""
+    val tech = Tech(name, email, phone, county, techLevel)
+    println(tech)
+    val xmlContent = """
+        <Tech>
+            <Name>${tech.name}</Name>
+            <Email>${tech.email}</Email>
+            <Phone>${tech.phone}</Phone>
+            <County>${tech.county}</County>
+            <TechLevel>${tech.techLevel}</TechLevel>
+        </Tech>
+    """.trimIndent()
+
+    // xml wil be writen and called tech.xml
+    val file = File("tech.xml")
+    file.writeText(xmlContent)
+
+    println("Tech details have been saved to file")
+}
