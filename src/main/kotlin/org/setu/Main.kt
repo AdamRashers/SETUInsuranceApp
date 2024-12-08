637378477c15105af0f6a2de64984697ef4935b1
Adam Raher <40344154+AdamRashers@users.noreply.github.com>
2024-12-08 14:30:49 +0000

    Removed the code for the inital xml test and added call functions

--- src/main/kotlin/org/setu/Main.kt
+++ src/main/kotlin/org/setu/Main.kt
@@ -1,34 +1,4 @@
-package org.setu
-
-import java.io.File
-
 fun main() {
-    println("Enter Tech details:")
-    print("Name: ")
-    val name = readLine() ?: ""
-    print("Email: ")
-    val email = readLine() ?: ""
-    print("Phone: ")
-    val phone = readLine() ?: ""
-    print("County: ")
-    val county = readLine() ?: ""
-    print("Tech Level: ")
-    val techLevel = readLine()?: ""
-    val tech = Tech(name, email, phone, county, techLevel)
-    println(tech)
-    val xmlContent = """
-        <Tech>
-            <Name>${tech.name}</Name>
-            <Email>${tech.email}</Email>
-            <Phone>${tech.phone}</Phone>
-            <County>${tech.county}</County>
-            <TechLevel>${tech.techLevel}</TechLevel>
-        </Tech>
-    """.trimIndent()
-
-    // xml wil be writen and called tech.xml
-    val file = File("tech.xml")
-    file.writeText(xmlContent)
-
-    println("Tech details have been saved to file")
+    val userInterface = MainUserInterface()
+    userInterface.displayWelcomeScreen()
 }
