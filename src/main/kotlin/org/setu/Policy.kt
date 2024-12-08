4d0b5d86a31ecbdae1e8e1452f41448b3af783e0
Adam Raher <40344154+AdamRashers@users.noreply.github.com>
2024-12-08 21:59:45 +0000

    Added functionality so they correspond with the create policy and user

---
+++ src/main/kotlin/org/setu/Policy.kt
@@ -0,0 +1,21 @@
+package org.setu
+
+data class Policy(
+    val car: String,
+    val user: User,  // User is a data class now
+    val price: Double,
+    val policyNumber: Int?
+) {
+    val monthlyCost: Double = price / 12
+
+    override fun toString(): String {
+        return "Car: $car\nName: ${user.name}\nEmail: ${user.email}\nPhone: ${user.phone}\nCounty: ${user.county}\nAnnual Price: $price\nMonthly Cost: $monthlyCost\nPolicy Number: $policyNumber\n\n"
+    }
+}
+
+data class User(
+    val name: String,
+    val email: String,
+    val phone: String,  // Add phone number property (optional)
+    val county: String  // Add county property (optional)
+)
--- src/main/kotlin/org/setu/Tech.kt
+++ src/main/kotlin/org/setu/Tech.kt
@@ -1,10 +1,11 @@
-package org.setu
-
 data class Tech(
     val name: String,
     val email: String,
     val phone: String,
     val county: String,
     val techLevel: String
-)
-
+) {
+    override fun toString(): String {
+        return "$name\t$email\t$phone\t$county\t$techLevel"
+    }
+}
